3949a4cb3f99a907b98dc0697fd7919a3eac5a50
Adailton Júnior <adailtonjunior609@gmail.com>
2023-02-26 19:35:30 -0300

    junior

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -12,10 +12,61 @@ const taskController = {
 
             const response = await TaskModel.create(task);
 
-            res.status(201).json({ response, msg: "Tarefa criada com sucesso" })
+            res.status(201).json({ response, msg: "Tarefa criada com sucesso" });
 
         } catch (error) {
-            res.status(500).json({ msg: "Erro ao comunicar com o servidor" })
+            res.status(500).json({ msg: "Erro ao comunicar com o servidor" });
+        }
+    },
+    getAll: async (req, res) => {
+        try {
+            const response = await TaskModel.find();
+
+            res.status(200).json(response);
+
+        } catch (error) {
+            res.status(500).json({ msg: "Erro ao comunicar com o servidor" });
+        }
+    },
+    get: async (req, res) => {
+        try {
+            const id = req.params.id;
+            const response = await TaskModel.findById(id);
+            res.status(200).json(response);
+        } catch (error) {
+            res.status(500).json({ msg: "Erro ao comunicar com o servidor" });
+        }
+    },
+    upadate: async (req, res) => {
+        try {
+            const id = req.body._id;
+            const task = {
+                title: req.body.title,
+                description: req.body.description,
+                date: req.body.date,
+                isLate: req.body.isLate,
+            }
+            const response = await TaskModel.findByIdAndUpdate(id, task);
+
+            if (!response) {
+                res.status(400).json({ msg: "O id informado não foi encontrado na base!" });
+            }
+            res.status(200).json({ msg: "A tarefa foi alterada com sucesso!" });
+        } catch (error) {
+            res.status(500).json({ msg: "Erro ao comunicar com o servidor" });
+        }
+    },
+    delete: async (req, res) => {
+        try {
+            const id = req.params.id;
+            console.log(id)
+            const response = await TaskModel.findByIdAndDelete(id);
+            if (!response) {
+                response.status(400).json({ msg: "O id informado não foi encontrado na base!" });
+            }
+            res.status(200).json({ msg: "A tarefa foi deletada com sucesso!" });
+        } catch (error) {
+            res.status(500).json({ msg: "Erro ao comunicar com o servidor" });
         }
     }
 };
--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -2,9 +2,16 @@ const router = require("express").Router();
 
 const taskController = require("../controllers/taskController");
 
-router.
-route("/task").
-post((req, res) => taskController.create(req, res))
+
+router.route("/task").post((req, res) => taskController.create(req, res));
+
+router.route("/task").get((req, res) => taskController.getAll(req, res));
+
+router.route("/task/:id").get((req, res) => taskController.get(req, res));
+
+router.route("/task/:id").put((req, res) => taskController.upadate(req, res));
+
+router.route("task/:id").delete((req, res) => taskController.delete(req, res));
 
 module.exports = router;
 
