d16dbe7156fe009766b615aa85eba37e46b5f98c
Luis Gustavo Furtuozo Falci <78694561+LuisFalci@users.noreply.github.com>
2023-02-26 23:56:05 -0300

    Update tasks.js

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,7 +1,5 @@
 const router = require("express").Router();
-
 const taskController = require("../controllers/taskController");
-
 
 router.route("/task").post((req, res) => taskController.create(req, res));
 
